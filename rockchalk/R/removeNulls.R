61bd4f9101cb80336174a0dfe58656e04ec215d3
Paul E. Johnson <pauljohn@ku.edu>
2018-09-07 12:41:37 -0500

    removeNulls.R: small util for lists

---
+++ rockchalk/R/removeNulls.R
@@ -0,0 +1,23 @@
+
+##' Remove NULL values variables from a list
+##'
+##' Unlike vectors, lists can hold objects with
+##' value NULL. This gets rid of them.
+##'
+##' This version is NOT recursive
+##'
+##' plyr::rbind.fill uses an experimental function
+##' that I choose to avoid. This is the "safe" version.
+##' 
+##' @param aList A list
+##' @return Same list with NULL's removed
+##' @author Paul Johnson
+##' @examples
+##' ## Note it is non-recursive, NULL remains in e
+##' x <- list(a = rnorm(5), b = NULL, c = rnorm(5), d = NULL,
+##'      e = list(f = rnorm(2), g = NULL))
+##' x
+##' removeNULL(x)
+removeNULL <- function (aList){ 
+    Filter(Negate(is.null), aList)
+}
