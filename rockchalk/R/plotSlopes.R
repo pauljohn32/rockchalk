efbf062858385ad8b138370fed3e979e6d992a0e
Paul E. Johnson <pauljohn@ku.edu>
2019-02-06 11:53:41 -0600

    plotSlopes: modxVals = "table" was not handled correctly

--- rockchalk/R/plotSlopes.R
+++ rockchalk/R/plotSlopes.R
@@ -1,13 +1,16 @@
 ##' Generic function for plotting regressions and interaction effects
 ##'
-##' This is a generic function for plotting regression
+##' This is a function for plotting regression
 ##' objects. So far, there is an implementation for \code{lm()} objects.
-##' This allows interaction effects, but not nonlinearities like log(x1).
-##' For that, please see \code{plotCurves}.
-##'
+##' I've been revising plotSlopes so that it should handle the work
+##' performed by plotCurves.  As sure as that belief is verified,
+##' the plotCurves work will be handled by plotSlopes. Different
+##' plot types are created, depending on whether the x-axis predictor
+##' \code{plotx} is numeric or categorical.
+##' ##'
 ##' @param model Required. A fitted Regression
 ##' @param plotx Required. Name of one predictor from the fitted
-##' model to be plotted on horizontal axis
+##' model to be plotted on horizontal axis. May be numeric or factor.
 ##' @param ... Additional arguments passed to methods. Often includes
 ##' arguments that are passed to plot. Any arguments that customize
 ##' plot output, such as lwd, cex, and so forth, may be
@@ -32,43 +35,48 @@ plotSlopes <- function(model, plotx, ...) UseMethod("plotSlopes")
 
 ##' Plot predicted values for focal values of a moderator variable.
 ##'
-##' This is a "simple slope" plotter for linear regression objects
-##' that are created by \code{lm()}.  The function \code{plotCurves()}
-##' can handle nonlinear predictive equations and generalized linear
-##' models. The term "simple slopes" was coined by psychologists
-##' (Aiken and West, 1991; Cohen, et al 2002) for analysis of
-##' interaction effects for particular values of a moderating
-##' variable. The moderating variable may be continuous or
+##' This is a "simple slope" plotter for regression objects created by
+##' \code{lm()} or similar functions that have capable predict methods
+##' with newdata arguments. The term "simple slopes" was coined by
+##' psychologists (Aiken and West, 1991; Cohen, et al 2002) for
+##' analysis of interaction effects for particular values of a
+##' moderating variable. The moderating variable may be continuous or
 ##' categorical, lines will be plotted for focal values of that
 ##' variable.
 ##'
-##' This function works well with lm models in which the predictor
-##' formula includes interactions, but it does not work well with
-##' nonlinear predictors (log(x) and poly(x)).  For that, please use
-##' \code{plotCurves}. plotSlopes is needed only when one wants to
-##' create an output object that can be used as input for
-##' \code{testSlopes()}.
+##' The original \code{plotSlopes} did not work well with nonlinear
+##' predictors (log(x) and poly(x)). The separate function
+##' \code{plotCurves()} was created for nonlinear predictive equations
+##' and generalized linear models, but the separation of the two
+##' functions was confusing for users. I've been working to make
+##' plotSlopes handle everything and \code{plotCurves} will disappear
+##' at some point.  \code{plotSlopes} can create an object which
+##' is then tested with \code{testSlopes()} and that can be graphed
+##' by a plot method.
 ##'
 ##' The argument \code{plotx} is the name of the horizontal plotting
-##' variable; it must be numeric.  The argument \code{modx} is the
-##' moderator variable. It may be either a numeric or a factor
-##' variable. As of version 1.7, the modx argument may be omitted. A
-##' single predicted value line will be drawn. That version also
-##' introduced the arguments interval and n.
+##' variable. An innovation was introduced in Version 1.8.33 so that
+##' \code{plotx} can be either  numeric or categorical.
+##'
+##' The argument \code{modx} is the moderator variable. It may be
+##' either a numeric or a factor variable. As of version 1.7, the modx
+##' argument may be omitted. A single predicted value line will be
+##' drawn. That version also introduced the arguments interval and n.
 ##'
 ##' There are many ways to specify focal values using the arguments
 ##' \code{modxVals} and \code{n}. This changed in rockchalk-1.7.0.  If
-##' \code{modxVals} is omitted, a default algorithm will be used,
-##' selecting \code{n} values for plotting. \code{modxVals} may be a
-##' vector of values (for a numeric moderator) or levels (for a
-##' factor).  If modxVals is a vector of values, then the argument
-##' \code{n} is ignored.  However, if modxVals is one of the name of
-##' one of the algorithms, "table", "quantile", or "std.dev.", then
-##' the argument \code{n} sets number of focal values to be selected.
-##' For numeric \code{modx}, n defaults to 3, but for factors
-##' \code{modx} will be the number of observed values of
-##' \code{modx}. If modxVals is omitted, the defaults will be used
-##' ("table" for factors, "quantile" for numeric variables).
+##' \code{modxVals} is omitted, a default algorithm for the variable
+##' type will be used to select \code{n} values for
+##' plotting. \code{modxVals} may be a vector of values (for a numeric
+##' moderator) or levels (for a factor).  If modxVals is a vector of
+##' values, then the argument \code{n} is ignored.  However, if
+##' modxVals is one of the name of one of the algorithms, "table",
+##' "quantile", or "std.dev.", then the argument \code{n} sets number
+##' of focal values to be selected.  For numeric \code{modx}, n
+##' defaults to 3, but for factors \code{modx} will be the number of
+##' observed values of \code{modx}. If modxVals is omitted, the
+##' defaults will be used ("table" for factors, "quantile" for numeric
+##' variables).
 ##'
 ##' For the predictors besides \code{modx} and \code{plotx} (the ones
 ##' that are not explicitly included in the plot), predicted values
@@ -101,7 +109,6 @@ plotSlopes <- function(model, plotx, ...) UseMethod("plotSlopes")
 ##' @param legendArgs Set as "none" if no legend is
 ##'     desired. Otherwise, this can be a list of named arguments that
 ##'     will override the settings I have for the legend.
-
 ##' @param col Optional.I offer my preferred color vector as default.
 ##'     Replace if you like. User may supply a vector of valid
 ##'     color names, or  \code{rainbow(10)} or
@@ -131,9 +138,11 @@ plotSlopes <- function(model, plotx, ...) UseMethod("plotSlopes")
 ##'     values of the moderator for which lines were drawn, and the
 ##'     color vector. It also includes the call that generated the
 ##'     plot.
-##' @references Aiken, L. S. and West, S.G. (1991). Multiple
-##'     Regression: Testing and Interpreting Interactions. Newbury
-##'     Park, Calif: Sage Publications.
+##' @references
+##'
+##' Aiken, L. S. and West, S.G. (1991). Multiple
+##' Regression: Testing and Interpreting Interactions. Newbury
+##' Park, Calif: Sage Publications.
 ##'
 ##' Cohen, J., Cohen, P., West, S. G., and Aiken, L. S. (2002).
 ##' Applied Multiple Regression/Correlation Analysis for the Behavioral
@@ -196,17 +205,19 @@ plotSlopes.lm <-
         plotxVals <- plotSeq(plotxRange, length.out = 40)
     }
 
-    if((missing(modxVals) || is.null(modxVals)) && !(missing(modx) || is.null(modx))){
-        modxVar <- mm[, modx]
-        if (is.factor(modxVar)) { ## modxVar is a factor
-            n <- ifelse(missing(n), nlevels(modxVar), n)
-            modxVar <- droplevels(modxVar)
-            modxVals <- getFocal(modxVar, xvals = modxVals, n, pct = legendPct)
-        } else {
-            modxVals <- getFocal(modxVar, xvals = modxVals, n, pct = legendPct)
+    if (!(missing(modx)) || is.null(modx)){
+        if((missing(modxVals) || is.null(modxVals)) || (length(modxVals) == 1 && is.character(modxVals))){
+            modxVar <- mm[, modx]
+            if (is.factor(modxVar)) { ## modxVar is a factor
+                n <- ifelse(missing(n), nlevels(modxVar), n)
+                modxVar <- droplevels(modxVar)
+                modxVals <- getFocal(modxVar, xvals = modxVals, n, pct = legendPct)
+            } else {
+                modxVals <- getFocal(modxVar, xvals = modxVals, n, pct = legendPct)
+            }
         }
-    } 
-    
+    }
+            
     predVals <- list(plotxVals)
     names(predVals) <- plotx
     if(!is.null(modx)) {
@@ -523,7 +534,7 @@ NULL
 ##' @param opacity Value in c(0, 254). 120 is default, that's partial see through.
 ##' @param lwd line width, usually 1
 ##' @param lty line type, usually 1
-##' @return NONE
+##' @return NULL
 ##' @author Paul Johnson
 ##' 
 se.bars <- function(x, y, lwr, upr, width = 0.20, col, opacity = 120, lwd = 1, lty = 1) {
@@ -594,7 +605,8 @@ plotFancyCategories <- function(newdf, olddf, plotx, modx=NULL,
     dotargs <- list(...)
     dotnames <- names(dotargs)
     lty <- if("lty" %in% dotnames)  dotargs[["lty"]] else 1
-   
+    interval <-  if("interval" %in% dotnames)  dotargs[["interval"]] else "none"
+    
     plotxval <- newdf[[plotx]]
     plotxVals <- levels(plotxval)
 
@@ -605,6 +617,7 @@ plotFancyCategories <- function(newdf, olddf, plotx, modx=NULL,
     ##Caution, modxVals comes out in wrong order compared to levels.
     ## Why? well put back in order according to levels(newdf)
     if(!is.null(modx)){
+        if(is.null(names(modxVals))) names(modxVals) <- as.character(modxVals) 
         modxVal.names <- names(modxVals)
         names(modxVal.names) <- modxVals
         modxVal.names <- modxVal.names[levels(newdf[[modx]])]
@@ -728,7 +741,7 @@ plotFancyCategories <- function(newdf, olddf, plotx, modx=NULL,
         #}
     }
 
-    if(is.character(gridArgs) && gridArgs == "none"){
+    if((is.character(gridArgs) && gridArgs == "none") || interval == "none"){
         ## do nothing
     } else {
         gridlwd <- rep(gridArgs[["lwd"]], length.out = length(lnc))
